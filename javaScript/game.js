aaf14f892549352ca695f025b3101196db38ffb8
Di Gandy <digandy@Dis-MacBook-Pro.lan>
2017-03-21 23:48:39 -0500

    almost finished

---
+++ javaScript/game.js
@@ -0,0 +1,26 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.js"></script>
+
+  <script type="text/javascript">
+
+    // Coin Flip JavaScript
+    // These variables are declared for us.
+    var cady;
+    var regina;
+    var gretchen;
+    var karen;
+    var wins=0;
+    var losses = 0;
+    var ranNum;
+
+    $(function() {
+      ranNum=Math.floor(Math.random(19,120));
+      $("#numberIs").html(ranNum);
+    }
+});
+
+      
+
+
+
+
+</script>
